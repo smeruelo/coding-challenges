eba30516b6d79e3404233055e3f004f4c535f102
Sonia Meruelo <smeruelo@gmail.com>
2018-04-15 17:35:11 +0200

    Improve 'ABC 094' (c)

--- atcoder/atcoder_beginner_094/c_many_medians.py
+++ atcoder/atcoder_beginner_094/c_many_medians.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 # https://abc094.contest.atcoder.jp/tasks/arc095_a
 
-def medians(nums):
-    n = len(nums)
-    num_orig_pos = list(zip(nums, range(n)))
-    nums_sorted = sorted(num_orig_pos)
-    medians = []
-    med_left = nums_sorted[(n + 1)//2][0]
-    med_right = nums_sorted[(n + 1)//2 - 1][0]
-    for i in range((n + 1)//2):
-        medians.append((med_left, nums_sorted[i][1]))
-    for i in range((n + 1)//2, n):
-        medians.append((med_right, nums_sorted[i][1]))
-    return map(lambda x: x[0], sorted(medians, key=lambda x: x[1]))
+def medians(nums, n):
+    median_left = nums[(n + 1)//2]
+    median_right = nums[(n + 1)//2 - 1]
+    return median_left, median_right
 
 
 if __name__ == '__main__':
-    _ = input()
+    n = int(input())
     nums = list(map(int, input().split()))
-    for i in medians(nums):
-        print(i)
+    median_left, median_right = medians(sorted(nums), n)
+    for i in nums:
+        print(median_left) if i < median_left else print(median_right)
