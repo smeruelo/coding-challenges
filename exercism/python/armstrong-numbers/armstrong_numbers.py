dd7f6bb2daff0eb9be2e37044abc60eadfb8e503
Sonia Meruelo <smeruelo@gmail.com>
2019-02-18 16:58:00 +0100

    exercism, python: armstrong numbers (more pythonic way)

--- exercism/python/armstrong-numbers/armstrong_numbers.py
+++ exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+# https://exercism.io/my/solutions/c0c3120a9b0e43abbb8505d8f8e29448
+
+
 def is_armstrong(number):
     n = str(number)
-    return sum(map(lambda d: pow(int(d), len(n)), n)) == number
+    return sum([int(d) ** len(n) for d in n]) == number
