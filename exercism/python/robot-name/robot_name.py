3fff1a02b6c18b12bd4ed2929954dbf3057f8ccd
Sonia Meruelo <smeruelo@gmail.com>
2019-02-18 12:06:29 +0100

    exercism, python: robot name (more efficient solution)

--- exercism/python/robot-name/robot_name.py
+++ exercism/python/robot-name/robot_name.py
@@ -1,25 +1,24 @@
+#!/usr/bin/python3
+# https://exercism.io/my/solutions/34f68f29449346e59df4e6d94d2f217f
+
+from itertools import product
+from random import shuffle
 from string import ascii_uppercase, digits
-from random import choices
 
 
-MAX_NAMES = pow(len(ascii_uppercase), 2) * pow(len(digits), 3)
-existing_robots = set()
+letters = [''.join(p) for p in product(ascii_uppercase, ascii_uppercase)]
+numbers = [''.join(p) for p in product(digits, digits, digits)]
+names = [l + n for l, n in product(letters, numbers)]
+shuffle(names)
+NAMES = iter(names)
 
 
 class Robot(object):
     def __init__(self):
-        self.name = self.assign_name()
-
-    def assign_name(self):
-        if len(existing_robots) < MAX_NAMES:
-            name = ''.join(choices(ascii_uppercase, k=2) + choices(digits, k=3))
-            while name in existing_robots:
-                name = ''.join(choices(ascii_uppercase, k=2) + choices(digits, k=3))
-            existing_robots.add(name)
-            self.name = name
-            return name
-        else:
-            raise Exception("No unique names available.")
+        self.reset()
 
     def reset(self):
-        self.name = self.assign_name()
+        try:
+            self.name = next(NAMES)
+        except StopIteration:
+            raise Exception("No unique names available.")
