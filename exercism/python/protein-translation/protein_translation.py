3dd5733c64ef6b8aa00e68867a3e97b7b6943bd1
Sonia Meruelo <smeruelo@gmail.com>
2019-02-18 18:09:38 +0100

    exercism, python: protein translation

---
+++ exercism/python/protein-translation/protein_translation.py
@@ -0,0 +1,31 @@
+PROTEINS = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAU': 'Tyrosine',
+    'UAC': 'Tyrosine',
+    'UGU': 'Cysteine',
+    'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP',
+    'UAG': 'STOP',
+    'UGA': 'STOP'
+}
+
+
+def proteins(strand):
+    codons = [strand[i:i+3] for i in range(0, len(strand), 3)]
+    translation = [PROTEINS[c] for c in codons]
+    output = []
+    for p in translation:
+        if p != 'STOP':
+            output.append(p)
+        else:
+            return output
+    return output
