69b2420302615848e28460cbbbd79dc1ab4fbd23
Sonia Meruelo <smeruelo@gmail.com>
2019-03-07 00:34:44 +0100

    exercism, python: largest series product (reuse 'series' code)

--- exercism/python/largest-series-product/largest_series_product.py
+++ exercism/python/largest-series-product/largest_series_product.py
@@ -4,13 +4,15 @@ from functools import reduce
 from operator import mul
 
 
-def largest_product(series, size):
+def slices(series, size):
     if len(series) < size or size < 0:
         raise ValueError('Invalid size.')
 
-    digits = [int(c) for c in series]
-    max_product = 0
-    for j in range(size, len(digits) + 1):
-        product = reduce(mul, digits[j-size:j], 1)
-        max_product = max(product, max_product)
-    return max_product
+    return [series[i:i+size] for i in range(len(series) - size + 1)]
+
+
+def largest_product(series, size):
+    def product(s):
+        return reduce(mul, map(int, s), 1)
+
+    return max(map(product, slices(series, size)))
