fa5b8f54f9a3b25ecd9567207db81827c4e4f618
Sonia Meruelo <smeruelo@gmail.com>
2019-04-19 22:08:06 +0200

    exercism, python: saddle points (simpler version) (less efficient)

--- exercism/python/saddle-points/saddle_points.py
+++ exercism/python/saddle-points/saddle_points.py
@@ -1,36 +1,19 @@
 # https://exercism.io/my/solutions/3eb1436fb16a4c1b88d3345b0f572316
 
 
-def irregular(matrix):
-    """Returns True if not all rows of the given matrix have the same length."""
-
-    if matrix == []:
-        return False
-    l = len(matrix[0])
-    return any([len(row) != l for row in matrix])
-
-
 def saddle_points(matrix):
     """Returns a set of positions in the given matrix where its values are saddle points."""
 
-    if matrix == []:
-        return set()
-
-    if irregular(matrix):
+    if len(set([len(row) for row in matrix])) > 1:
         raise ValueError('Matrix rows are not equal in size.')
 
-    rows_max = [matrix[row][0] for row in range(len(matrix))]
-    cols_min = matrix[0]
-    for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
-            if matrix[row][col] > rows_max[row]:
-                rows_max[row] = matrix[row][col]
-            if matrix[row][col] < cols_min[col]:
-                cols_min[col] = matrix[row][col]
+    matrix_t = list(map(list, zip(*matrix)))
+    rows_max = [max(row) for row in matrix]
+    cols_min = [min(row) for row in matrix_t]
 
     saddle_points = set()
     for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
+        for col in range(len(matrix_t)):
             if matrix[row][col] == rows_max[row] and matrix[row][col] == cols_min[col]:
                 saddle_points.add((row + 1, col + 1))
     return saddle_points
