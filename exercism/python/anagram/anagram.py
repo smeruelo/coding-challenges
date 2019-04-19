8fe1cf3c585e1f3ff7e8b3a871baf3e09989d831
Sonia Meruelo <smeruelo@gmail.com>
2019-04-19 19:09:56 +0200

    exercism, python: anagram (simpler)

--- exercism/python/anagram/anagram.py
+++ exercism/python/anagram/anagram.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 # https://exercism.io/my/solutions/610deb702acc45ed859259f2e737f45c
 
-from collections import Counter
-
 
 def find_anagrams(word, candidates):
     w = word.lower()
-    c = Counter(w)
-    return list(filter(lambda x: x.lower() != w and Counter(x.lower()) == c, candidates))
+    s = sorted(w)
+    return list(filter(lambda c: c.lower() != w and sorted(c.lower()) == s, candidates))
