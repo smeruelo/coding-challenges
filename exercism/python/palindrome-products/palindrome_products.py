d3739f1b10de7faed16d47504b098053f0db60d5
Sonia Meruelo <smeruelo@gmail.com>
2019-04-22 01:12:12 +0200

    exercism, python: palindrome products (efficient)

--- exercism/python/palindrome-products/palindrome_products.py
+++ exercism/python/palindrome-products/palindrome_products.py
@@ -1,32 +1,36 @@
 # https://exercism.io/my/solutions/5a6b231b50ac4f6ab8f3391f87a62638
 
+from math import sqrt
+
 
 def is_palindrome(n):
     return int(str(n)[::-1]) == n
 
 
-def palindrome_and_factors(max_factor, min_factor, func):
+def palindrome_and_factors(max_factor, min_factor, largest=True):
     if max_factor < min_factor:
         raise ValueError('Invalid range')
 
-    f = []
-    v = None
-    for f1 in range(min_factor, max_factor + 1):
-        for f2 in range(f1, max_factor + 1):
-            product = f1 * f2
-            if product == v:
-                f.append((f1, f2))
-            elif (v is None or product == func(v, product)) and is_palindrome(product):
-                v = product
-                f = [(f1, f2)]
-    if v:
-        return v, set(f)
+    if largest:
+        range_values = (max_factor**2, min_factor**2 - 1, -1)
+    else:
+        range_values = (min_factor**2, max_factor**2 + 1)
+
+    for value in range(*range_values):
+        if is_palindrome(value):
+            factors = set()
+            for f1 in range(min_factor, int(sqrt(value)) + 1):
+                f2 = value // f1
+                if value % f1 == 0 and f2 <= max_factor:
+                    factors.add((f1, f2))
+            if factors:
+                return value, factors
     return None, []
 
 
 def largest_palindrome(max_factor, min_factor):
-    return palindrome_and_factors(max_factor, min_factor, max)
+    return palindrome_and_factors(max_factor, min_factor, largest=True)
 
 
 def smallest_palindrome(max_factor, min_factor):
-    return palindrome_and_factors(max_factor, min_factor, min)
+    return palindrome_and_factors(max_factor, min_factor, largest=False)
