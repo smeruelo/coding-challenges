9e4ce94097a885a8cf25c2fb1f96b43785ba9740
Sonia Meruelo <smeruelo@gmail.com>
2019-04-21 23:59:09 +0200

    exercism, python: palindrome and factors
    No dictionary
    No duplicated code
    Less inefficient

--- exercism/python/palindrome-products/palindrome_products.py
+++ exercism/python/palindrome-products/palindrome_products.py
@@ -1,32 +1,32 @@
-from collections import defaultdict
+# https://exercism.io/my/solutions/5a6b231b50ac4f6ab8f3391f87a62638
 
 
 def is_palindrome(n):
     return int(str(n)[::-1]) == n
 
 
-def palindromes(max_factor, min_factor):
+def palindrome_and_factors(max_factor, min_factor, func):
     if max_factor < min_factor:
         raise ValueError('Invalid range')
 
-    p = defaultdict(set)
+    f = []
+    v = None
     for f1 in range(min_factor, max_factor + 1):
         for f2 in range(f1, max_factor + 1):
             product = f1 * f2
-            if is_palindrome(product):
-                p[product].add((f1, f2))
-    return p
+            if product == v:
+                f.append((f1, f2))
+            elif (v is None or product == func(v, product)) and is_palindrome(product):
+                v = product
+                f = [(f1, f2)]
+    if v:
+        return v, set(f)
+    return None, []
 
 
 def largest_palindrome(max_factor, min_factor):
-    p = palindromes(max_factor, min_factor)
-    if p:
-        return max(p), p[max(p)]
-    return None, []
+    return palindrome_and_factors(max_factor, min_factor, max)
 
 
 def smallest_palindrome(max_factor, min_factor):
-    p = palindromes(max_factor, min_factor)
-    if p:
-        return min(p), p[min(p)]
-    return None, []
+    return palindrome_and_factors(max_factor, min_factor, min)
