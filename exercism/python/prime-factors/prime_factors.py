7acbfcdb70a3b0bbc055e0eed794a9b020514a20
Sonia Meruelo <smeruelo@gmail.com>
2019-04-22 17:54:32 +0200

    exercism, python: prime factors (more efficient)

--- exercism/python/prime-factors/prime_factors.py
+++ exercism/python/prime-factors/prime_factors.py
@@ -1,32 +1,14 @@
-from math import sqrt
-
-
-def is_prime(x):
-    for i in range(2, int(sqrt(x)) + 1):
-        if x % i == 0:
-            return False
-    return True
-
-
-def primes():
-    yield 2
-    i = 3
-    while True:
-        if is_prime(i):
-            yield i
-        i += 2
+# https://exercism.io/my/solutions/27867a1fd02e49b5a4de832109d34149
 
 
 def prime_factors(number):
     factors = []
-    prime_gen = primes()
-
-    f = next(prime_gen)
+    f = 2
     while number != 1:
         if number % f == 0:
             factors.append(f)
             number = number // f
         else:
-            f = next(prime_gen)
+            f += 1
 
     return factors
