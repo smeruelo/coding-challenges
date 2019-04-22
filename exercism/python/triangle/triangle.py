e6afcabff92603f2854998abc14bd704dec5c03b
Sonia Meruelo <smeruelo@gmail.com>
2019-04-22 15:25:47 +0200

    exercism, python: triangle (decorator)

--- exercism/python/triangle/triangle.py
+++ exercism/python/triangle/triangle.py
@@ -1,23 +1,23 @@
 # https://exercism.io/my/solutions/0b66635e96c4419b97da8e2ec35d7ed3
 
 
-def is_valid(sides):
-    return all(sides) and \
+def triangle(f):
+    return lambda sides: all(sides) and \
         sides[0] + sides[1] >= sides[2] and \
         sides[1] + sides[2] >= sides[0] and \
-        sides[0] + sides[2] >= sides[1]
+        sides[0] + sides[2] >= sides[1] and f(sides)
 
 
+@triangle
 def is_equilateral(sides):
-    return is_valid(sides) and \
-        sides[0] == sides[1] and sides[1] == sides[2]
+    return len(set(sides)) == 1
 
 
+@triangle
 def is_isosceles(sides):
-    return is_valid(sides) and \
-        (sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2])
+    return len(set(sides)) <= 2
 
 
+@triangle
 def is_scalene(sides):
-    return is_valid(sides) and \
-        sides[0] != sides[1] and sides[1] != sides[2] and sides[0] != sides[2]
+    return len(set(sides)) == 3
