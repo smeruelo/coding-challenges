0b89ece565c039d409b7d10bd5378582e5bb4e8f
Sonia Meruelo <smeruelo@gmail.com>
2019-04-30 12:04:33 +0200

    exercism, python: isogram (improvements)
    Improvements after mentor's feedback

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 # https://exercism.io/my/solutions/55d439aa234544dcb5a8cc75b776dd7b
 
-from string import ascii_lowercase
-
 
 def is_isogram(string):
-    letters = [c for c in str.lower(string) if c in ascii_lowercase]
+    letters = [c for c in string.lower() if c.isalpha()]
     return len(letters) == len(set(letters))
