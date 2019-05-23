505453844cf5618d2be13dfa35e90ef5ff77a97f
Sonia Meruelo <smeruelo@gmail.com>
2019-05-23 14:55:01 +0200

    cses: two knights (O(n), iterative)

--- cses/two knights/two_knights.py
+++ cses/two knights/two_knights.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 # https://cses.fi/problemset/task/1072
 
-import sys
-sys.setrecursionlimit(12000)
 
-
-def knights(k):
-    border_cells = (k * 2) - 1
-
-    def two_in_border():
+def knights(n):
+    def two_in_border(k, border_cells):
         threatened = 0 if k < 3 else 2
         return ((border_cells - 1) * border_cells) // 2 - threatened
 
-    def one_in_border_one_inside():
+    def one_in_border_one_inside(k, border_cells):
         threatened = 0 if k < 3 else 2 + 2 * (10 + 4 * (k - 5))
         return (k - 1) * (k - 1) * border_cells - threatened
 
-    if k == 1:
-        return 0
-    else:
-        two_inside = knights(k - 1)
-        print(two_inside)
-        return two_in_border() + one_in_border_one_inside() + two_inside
+    output = [0]
+    for k in range(2, n + 1):
+        border_cells = (k * 2) - 1
+        output.append(two_in_border(k, border_cells) +
+                      one_in_border_one_inside(k, border_cells) +
+                      output[-1])
+    return output
 
 
 if __name__ == '__main__':
-    print(knights(int(input())))
+    print('\n'.join(map(str, knights(int(input())))))
