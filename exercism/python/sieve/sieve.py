629dbd08774479e24fc640e6d416fd3027a2a68f
Sonia Meruelo <smeruelo@gmail.com>
2019-06-27 10:48:01 +0200

    exercism, python: sieve (remove unnecessary filter)

--- exercism/python/sieve/sieve.py
+++ exercism/python/sieve/sieve.py
@@ -1,9 +1,14 @@
+# https://exercism.io/my/solutions/71af6b0543e148b9af5c3b8f2af9eda7
+
+
 def primes(limit):
     m = [0] * (limit + 1)
-    for n in range(2, limit):
+    primes = []
+    for n in range(2, limit + 1):
         if m[n] == 0:
+            primes.append(n)
             multiple = 2 * n
             while multiple <= limit:
                 m[multiple] = n
                 multiple += n
-    return [i for i, n in enumerate(m) if n == 0 and i >= 2]
+    return primes
