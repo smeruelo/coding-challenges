30b45db22fe50a0a8c7e26a5f75610b971688b94
Sonia Meruelo <smeruelo@gmail.com>
2019-07-31 18:45:17 +0200

    exercism, common lisp: hamming (minor improvement)

--- exercism/common-lisp/hamming/hamming.lisp
+++ exercism/common-lisp/hamming/hamming.lisp
@@ -10,5 +10,5 @@
   "Number of positional differences in two equal length dna strands."
   (when (= (length dna1) (length dna2))
     (let ((c 0))
-      (map nil (lambda (c1 c2) (incf c (if (char= c1 c2) 0 1))) dna1 dna2)
+      (map nil (lambda (c1 c2) (when (char/= c1 c2) (incf c))) dna1 dna2)
       c)))
