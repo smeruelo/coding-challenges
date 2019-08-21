d31d4e10cf9f858c9fd13c58dd3ead4757d2c572
Sonia Meruelo <smeruelo@gmail.com>
2019-08-22 00:38:47 +0200

    exercism, python: go-counting (recursive)

--- exercism/python/go-counting/go_counting.py
+++ exercism/python/go-counting/go_counting.py
@@ -1,3 +1,7 @@
+BLACK = 'B'
+WHITE = 'W'
+NONE = ' '
+
 
 class Board:
     """Count territories of each player in a Go game
@@ -7,7 +11,38 @@ class Board:
     """
 
     def __init__(self, board):
-        pass
+        self._board = board
+        self._width = len(board[0])
+        self._height = len(board)
+
+    def is_valid(self, cell):
+        x, y = cell
+        return x >= 0 and x < self._width and y >= 0 and y < self._height
+
+    def neighbours(self, cell):
+        x, y = cell
+        return filter(lambda cell: self.is_valid(cell), [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)])
+
+    def explore(self, cell):
+        encircled = set()
+
+        def aux(cell):
+            x, y = cell
+            if self._board[y][x] == NONE:
+                encircled.add((x, y))
+                nexts_to_explore = filter(lambda c: not (c in encircled), self.neighbours(cell))
+                owners = [aux(c) for c in nexts_to_explore]
+                if owners and all(map(lambda o: o == BLACK, owners)):
+                    return BLACK
+                if owners and all(map(lambda o: o == WHITE, owners)):
+                    return WHITE
+                return NONE
+            return self._board[y][x]
+
+        owner = aux(cell)
+        if encircled:
+            return (owner, encircled)
+        return (NONE, set())
 
     def territory(self, x, y):
         """Find the owner and the territories given a coordinate on
@@ -23,7 +58,10 @@ class Board:
                         second being a set of coordinates, representing
                         the owner's territories.
         """
-        pass
+
+        if self.is_valid((x, y)):
+            return self.explore((x, y))
+        raise ValueError('Invalid coordinate')
 
     def territories(self):
         """Find the owners and the territories of the whole board
@@ -36,4 +74,18 @@ class Board:
                         , i.e. "W", "B", "".  The value being a set
                         of coordinates owned by the owner.
         """
-        pass
+
+        d = dict()
+        d[WHITE] = set()
+        d[BLACK] = set()
+        d[NONE] = set()
+        visited = set()
+
+        for y in range(self._height):
+            for x in range(self._width):
+                if not (x, y) in visited and self._board[y][x] == NONE:
+                    owner, encircled = self.territory(x, y)
+                    d[owner].update(encircled)
+                    visited.update(encircled)
+
+        return d
