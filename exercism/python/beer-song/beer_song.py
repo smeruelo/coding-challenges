05c6406033d679cd140a24151302883132307863
Sonia Meruelo <smeruelo@gmail.com>
2019-09-20 00:40:19 +0200

    exercism, python: more readable and simpler version

--- exercism/python/beer-song/beer_song.py
+++ exercism/python/beer-song/beer_song.py
@@ -1,26 +1,20 @@
 # https://exercism.io/my/solutions/a43b8babd915454c822b5f1bf143ebae
 
-LIMIT = 100
-
 
 def verse(i):
-    def bottles_str(i):
-        return 'no more bottles' if i < 1 else f'{i} bottle{"s" if i > 1 else ""}'
+    VERSE_N = [f'{i} bottles of beer on the wall, {i} bottles of beer.',
+               f'Take one down and pass it around, {i - 1} bottle{"s" if i > 2 else ""} of beer on the wall.']
+    VERSE_1 = ['1 bottle of beer on the wall, 1 bottle of beer.',
+               'Take it down and pass it around, no more bottles of beer on the wall.']
+    VERSE_0 = ['No more bottles of beer on the wall, no more bottles of beer.',
+               'Go to the store and buy some more, 99 bottles of beer on the wall.']
 
-    def take_str(i):
-        return 'one' if i > 1 else 'it'
-
-    before = bottles_str(i)
-    after = bottles_str((i - 1) % LIMIT)
-
-    first = f'{before[0].upper()}{before[1:]} of beer on the wall, {before} of beer.'
-    if i > 0:
-        second = f'Take {take_str(i)} down and pass it around'
+    if i > 1:
+        return VERSE_N
+    elif i == 1:
+        return VERSE_1
     else:
-        second = 'Go to the store and buy some more'
-    third = f'{after} of beer on the wall.'
-
-    return [first, f'{second}, {third}']
+        return VERSE_0
 
 
 def recite(start, take=1):
