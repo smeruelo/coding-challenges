6aeb8b62e49f4d347c5286473a7fc13ec2a26c75
Sonia Meruelo <smeruelo@gmail.com>
2019-11-15 11:46:54 +0100

    exercism, python: secret handshake (list comprehension)

--- exercism/python/secret-handshake/secret_handshake.py
+++ exercism/python/secret-handshake/secret_handshake.py
@@ -4,12 +4,6 @@ ACTIONS = ['wink', 'double blink', 'close your eyes', 'jump']
 
 
 def commands(number):
-    # Convert to binary, take the 5 less-significative bits (reversed)
-    s = format(number, 'b')[::-1][:5]
-
-    handshake = list(filter(lambda x: x,
-                            map(lambda c, action: action if c == '1' else None, s, ACTIONS)))
-    if len(s) == 5 and s[4] == '1':
-        return handshake[::-1]
-    else:
-        return handshake
+    s = format(number, 'b')[::-1]
+    handshake = [ACTIONS[i] for i, bit in enumerate(s[:4]) if bit == '1']
+    return handshake[::-1] if len(s) > 4 and s[4] == '1' else handshake
