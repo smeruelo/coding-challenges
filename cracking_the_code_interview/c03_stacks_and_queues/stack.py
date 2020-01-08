f9a50e4a97ca3df48a71a0753ec103d11ebba946
Sonia Meruelo <smeruelo@gmail.com>
2020-01-08 19:59:42 +0100

    cracking..: Add length to stacks

--- cracking_the_code_interview/c03_stacks_and_queues/stack.py
+++ cracking_the_code_interview/c03_stacks_and_queues/stack.py
@@ -5,24 +5,32 @@ class Stack():
     def __init__(self):
         self._lst = MSinglyLinkedList()
 
+    # O(1)
     def pop(self):
         try:
             return self._lst.pop()
         except Exception:
             raise EmptyStackError
 
+    # O(1)
     def push(self, item):
         self._lst.push(item)
         return self
 
+    # O(1)
     def peek(self):
         try:
             return self._lst.peek()
         except Exception:
             raise EmptyStackError
 
+    # O(1)
     def is_empty(self):
         return self._lst.is_empty()
+
+    # O(n)
+    def length(self):
+        return self._lst.length()
 
     def __repr__(self):
         return self._lst.__repr__()
