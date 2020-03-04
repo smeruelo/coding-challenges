80a04451852ed71df76941122ce4b91e4b39a8d2
Sonia Meruelo <smeruelo@gmail.com>
2020-03-04 10:57:30 +0100

    codechef, WICP2002: Day 4, selection battle

---
+++ codechef/WICP2002/04_selection_battle/selection_battle.py
@@ -0,0 +1,23 @@
+from math import sqrt
+from sys import stdin
+
+
+def read():
+    input_data = stdin.read().split()
+    for token in input_data:
+        yield token
+
+
+def is_prime(n):
+    for i in range(int(sqrt(n)), 1, -1):
+        if n % i == 0:
+            return False
+    return True
+
+
+reader = read()
+for _ in range(int(next(reader))):
+    if is_prime(int(next(reader))):
+        print('Divesh')
+    else:
+        print('Tanya')
