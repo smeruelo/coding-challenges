d3e3e3d9e2cd56e0a46c2c9bce911f8f95dda2b0
Sonia Meruelo <smeruelo@gmail.com>
2020-03-18 18:07:43 +0100

    exercism, python: tournament (mentor's feedback)
    Use Team class

--- exercism/python/tournament/tournament.py
+++ exercism/python/tournament/tournament.py
@@ -10,40 +10,60 @@ WIN = 'win'
 DRAW = 'draw'
 LOSS = 'loss'
 
-TeamData = namedtuple('TeamData', ['won', 'drawn', 'lost', 'points'], defaults=(0, 0, 0, 0))
+
+class Team():
+    def __init__(self, name):
+        self.name = name
+        self.won = 0
+        self.drawn = 0
+        self.lost = 0
+
+    def matches_played(self):
+        return self.won + self.drawn + self.lost
+
+    def points(self):
+        return self.won * WIN_POINTS + self.drawn * DRAW_POINTS + self.lost * LOSS_POINTS
+
+    def __lt__(self, other):
+        if self.points() == other.points():
+            return self.name > other.name
+        else:
+            return self.points() < other.points()
 
 
 def tally(rows):
 
-    def add_team_result(data, team, result):
-        current = data[team] if team in data else TeamData()
+    def update_teams(teams, name, result):
+        if name not in teams:
+            teams[name] = Team(name)
+        team = teams[name]
         if result == WIN:
-            data[team] = TeamData(current.won + 1, current.drawn, current.lost, current.points + WIN_POINTS)
+            team.won += 1
         elif result == DRAW:
-            data[team] = TeamData(current.won, current.drawn + 1, current.lost, current.points + DRAW_POINTS)
+            team.drawn += 1
         elif result == LOSS:
-            data[team] = TeamData(current.won, current.drawn, current.lost + 1, current.points + LOSS_POINTS)
+            team.lost += 1
         return
 
     def read_results(rows):
         other_team_result = {WIN:LOSS, DRAW:DRAW, LOSS:WIN}
-        data = dict()
+        teams = dict()
         for row in rows:
             team1, team2, result = row.split(';')
-            add_team_result(data, team1, result)
-            add_team_result(data, team2, other_team_result[result])
-        return data
+            update_teams(teams, team1, result)
+            update_teams(teams, team2, other_team_result[result])
+        return teams
 
-    def table(data):
-        def points_descending_name_ascending(team_and_data):
-            name, data = team_and_data
-            return (-data.points, name)
-
+    def table(teams):
         table = ['Team                           | MP |  W |  D |  L |  P']
-        for team, team_data in sorted(data.items(), key=points_descending_name_ascending):
-            won, drawn, lost, points = team_data
-            played = won + drawn + lost
-            table.append(f'{team:30} | {played:2} | {won:2} | {drawn:2} | {lost:2} | {points:2}')
+        for team in sorted(teams.values(), reverse=True):
+            matches_played = team.matches_played()
+            points = team.points()
+            row = (
+                f'{team.name:30} | {matches_played:2} | {team.won:2} | '
+                f'{team.drawn:2} | {team.lost:2} | {points:2}'
+            )
+            table.append(row)
         return table
 
     return table(read_results(rows))
