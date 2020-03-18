f4adf1eff8fcc775b453a799cfc73c70f9bd655c
Sonia Meruelo <smeruelo@gmail.com>
2020-03-18 19:24:57 +0100

    exercism, python: tournament (use dicts for matches)

--- exercism/python/tournament/tournament.py
+++ exercism/python/tournament/tournament.py
@@ -1,28 +1,22 @@
 # https://exercism.io/my/solutions/b3b91870e23c4ca7ae4ea6661ece1096
 
-from collections import namedtuple
-
-WIN_POINTS = 3
-DRAW_POINTS = 1
-LOSS_POINTS = 0
 
 WIN = 'win'
 DRAW = 'draw'
 LOSS = 'loss'
+POINTS = {WIN: 3, DRAW: 1, LOSS: 0}
 
 
 class Team():
     def __init__(self, name):
         self.name = name
-        self.won = 0
-        self.drawn = 0
-        self.lost = 0
+        self.matches = {WIN: 0, DRAW: 0, LOSS: 0}
 
     def matches_played(self):
-        return self.won + self.drawn + self.lost
+        return sum(self.matches.values())
 
     def points(self):
-        return self.won * WIN_POINTS + self.drawn * DRAW_POINTS + self.lost * LOSS_POINTS
+        return sum([POINTS[result] * count for result, count in self.matches.items()])
 
     def __lt__(self, other):
         if self.points() == other.points():
@@ -30,19 +24,23 @@ class Team():
         else:
             return self.points() < other.points()
 
+    def __str__(self):
+        return (
+            f'{self.name:30} | '
+            f'{self.matches_played():2} | '
+            f'{self.matches[WIN]:2} | '
+            f'{self.matches[DRAW]:2} | '
+            f'{self.matches[LOSS]:2} | '
+            f'{self.points():2}'
+        )
+
 
 def tally(rows):
 
     def update_teams(teams, name, result):
         if name not in teams:
             teams[name] = Team(name)
-        team = teams[name]
-        if result == WIN:
-            team.won += 1
-        elif result == DRAW:
-            team.drawn += 1
-        elif result == LOSS:
-            team.lost += 1
+        teams[name].matches[result] += 1
         return
 
     def read_results(rows):
@@ -57,13 +55,7 @@ def tally(rows):
     def table(teams):
         table = ['Team                           | MP |  W |  D |  L |  P']
         for team in sorted(teams.values(), reverse=True):
-            matches_played = team.matches_played()
-            points = team.points()
-            row = (
-                f'{team.name:30} | {matches_played:2} | {team.won:2} | '
-                f'{team.drawn:2} | {team.lost:2} | {points:2}'
-            )
-            table.append(row)
+            table.append(str(team))
         return table
 
     return table(read_results(rows))
