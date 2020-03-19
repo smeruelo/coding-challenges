7f3737db738a3ae055336871da4d2cb537ecd605
Sonia Meruelo <smeruelo@gmail.com>
2020-03-20 00:00:11 +0100

    exercism, python: roman numerals

---
+++ exercism/python/roman-numerals/roman_numerals.py
@@ -0,0 +1,20 @@
+def roman(number):
+    letters = ['IV', 'XL', 'CD', 'MV']
+
+    def one_digit(n, power):
+        ch1, ch5 = letters[power]
+        if n <= 3:
+            return ch1 * n
+        if n >= 4 and n <= 8:
+            return (ch1 * (5 - n)) + ch5 + (ch1 * (n - 5))
+        if n == 9:
+            if power > 2:
+                raise Exception('Number too large')
+            ch5 = letters[power+1][0]
+            return ch1 + ch5
+
+    s = ''
+    for i, digit in enumerate(str(number)):
+        power = len(str(number)) - 1 - i
+        s += one_digit(int(digit), power)
+    return s
