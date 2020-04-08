7b29c54606b80f4da42c7f36fae95c9a083874f8
Sonia Meruelo <smeruelo@gmail.com>
2020-04-08 12:25:01 +0200

    exercism, ocaml: leap year (solution)

--- exercism/ocaml/leap/leap.ml
+++ exercism/ocaml/leap/leap.ml
@@ -1,2 +1,6 @@
-let leap_year _ = 
-    failwith "'leap_year' is missing"
+(* https://exercism.io/my/solutions/ba211fe0f25a4d48b07489aef1668474 *)
+
+let leap_year year =
+  let multiple n =
+    year mod n == 0 in
+  multiple 4 && (not (multiple 100) || (multiple 400))
