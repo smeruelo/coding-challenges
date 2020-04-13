ffd8658207970681732ec53acfce7ae570cf7ea6
Sonia Meruelo <smeruelo@gmail.com>
2020-04-14 00:08:31 +0200

    exercism, python: spiral-matrix (use generator)

--- exercism/python/spiral-matrix/spiral_matrix.py
+++ exercism/python/spiral-matrix/spiral_matrix.py
@@ -1,29 +1,27 @@
 # https://exercism.io/my/solutions/0141222eca73472a9a2e2bcc0f31f6d9
 
+from itertools import count
+
 
 def spiral_matrix(size):
     m = [[None] * size for _ in range(size)]
-    def perimeter(length, i):
+    i = count(1)
+
+    def perimeter(length):
         row = col = (size - length) // 2
         r = row
         for c in range(col, col+length):
-            m[r][c] = i
-            i += 1
+            m[r][c] = next(i)
         c = col + length - 1
         for r in range(row+1, row+length):
-            m[r][c] = i
-            i += 1
+            m[r][c] = next(i)
         r = row + length - 1
         for c in range(col+length-2, col-1, -1):
-            m[r][c] = i
-            i += 1
+            m[r][c] = next(i)
         c = col
         for r in range(row+length-2, row, -1):
-            m[r][c] = i
-            i += 1
-        return i
+            m[r][c] = next(i)
 
-    i = 1
     for length in range(size, 0, -2):
-        i = perimeter(length, i)
+        perimeter(length)
     return m
