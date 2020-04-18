54b4c5577ca11ce5cae959a15183e82bfd6c169c
Sonia Meruelo <smeruelo@gmail.com>
2020-04-18 13:13:18 +0200

    exercism, python: rna translation (translation table

--- exercism/python/rna-transcription/rna_transcription.py
+++ exercism/python/rna-transcription/rna_transcription.py
@@ -1,10 +1,6 @@
 # https://exercism.io/my/solutions/f5c71bd8f317474395b0b86c85c4d797
 
-def to_rna(dna_strand):
-    COMPLEMENTS = {'A': 'U', 'C': 'G', 'G': 'C', 'T': 'A'}
-    
-    def complement(nucleotide):
-        return COMPLEMENTS[nucleotide]
+TABLE = str.maketrans('ACGT', 'UGCA')
 
-    return ''.join(map(complement, dna_strand))
-        
+def to_rna(dna_strand):
+    return dna_strand.translate(TABLE)
