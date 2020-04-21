8863bccffcb5daf29f6ada81e7da4c161fa6161a
Sonia Meruelo <smeruelo@gmail.com>
2020-04-21 16:13:41 +0200

    exercism, python: circular buffer (no pointers)

--- exercism/python/circular-buffer/circular_buffer.py
+++ exercism/python/circular-buffer/circular_buffer.py
@@ -8,37 +8,29 @@ class BufferEmptyException(Exception):
 
 class CircularBuffer:
     def __init__(self, capacity):
-        self._buffer = [None] * capacity
+        self._buffer = []
         self._capacity = capacity
-        self._write_index = 0
-        self._read_index = None
 
     def read(self):
-        if self._read_index is None:
+        if self._empty():
             raise BufferEmptyException("There's no data to read.")
-        data = self._buffer[self._read_index]
-        if self._write_index is None:
-            self._write_index = self._read_index
-        nxt = (self._read_index + 1) % self._capacity
-        self._read_index = nxt if nxt != self._write_index else None
-        return data
+        return self._buffer.pop(0)
 
     def write(self, data):
-        if self._write_index is None:
+        if self._full():
             raise BufferFullException("There's no free slot to write in.")
-        self._buffer[self._write_index] = data
-        if self._read_index is None:
-            self._read_index = self._write_index
-        nxt = (self._write_index + 1) % self._capacity
-        self._write_index = nxt if nxt != self._read_index else None
+        self._buffer.append(data)
 
     def overwrite(self, data):
-        if self._write_index is None:
-            self._buffer[self._read_index] = data
-            self._read_index = (self._read_index + 1) % self._capacity
-        else:
-            self.write(data)
+        if self._full():
+            _ = self._buffer.pop(0)
+        self._buffer.append(data)
 
     def clear(self):
-        self._write_index = 0
-        self._read_index = None
+        self._buffer = []
+
+    def _empty(self):
+        return len(self._buffer) == 0
+
+    def _full(self):
+        return len(self._buffer) >= self._capacity
