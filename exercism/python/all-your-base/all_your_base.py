9cbcc51c9f07ee22aa59cc3d2ec7ac2295a1b8ad
Sonia Meruelo <smeruelo@gmail.com>
2020-04-22 16:29:16 +0200

    exercism, python: all your base (improvements)

--- exercism/python/all-your-base/all_your_base.py
+++ exercism/python/all-your-base/all_your_base.py
@@ -1,24 +1,18 @@
+# https://exercism.io/my/solutions/96ac04438bcd4b90a89142a4198ce59f
+
 from math import log
 
 
 def to_decimal(input_base, digits):
-    decimal = 0
-    for power, digit in enumerate(reversed(digits)):
-        decimal += digit * (input_base ** power)
-    return decimal
+    return sum(digit * (input_base ** power) for power, digit in enumerate(reversed(digits)))
 
 
 def from_decimal(decimal, output_base):
-    if decimal == 0:
-        return [0]
-
-    max_power = int(log(decimal, output_base))
     output_digits = []
-    for power in range(max_power, -1, -1):
-        weight = output_base ** power
-        output_digits.append(decimal // weight)
-        decimal -= (decimal // weight) * weight
-    return output_digits
+    while decimal > 0:
+        output_digits.append(decimal % output_base)
+        decimal //= output_base
+    return list(reversed(output_digits)) if output_digits else [0]
 
 
 def rebase(input_base, digits, output_base):
