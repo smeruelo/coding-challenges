bd2e9d9003f595b91c0a5d7036a8bdf6df97825c
Sonia Meruelo <smeruelo@gmail.com>
2020-05-16 00:05:45 +0200

    exercism, python: change (dyn prog, iterative)

--- exercism/python/change/change.py
+++ exercism/python/change/change.py
@@ -1,35 +1,27 @@
 # https://exercism.io/my/solutions/1edf06f8150c4020a5c75f3586c3ba44
 
-import sys
-
-sys.setrecursionlimit(4000)
-
-
-def min_num_coins(coins, target, memo):
-    if target in memo:
-        return memo[target]
-
-    pos = list(filter(lambda x: x[0] is not None,
-                      map(lambda t: (None, None) if t[0] < 0 else (min_num_coins(coins, t[0], memo)[0], t[1]),
-                          ((target - c, c) for c in coins))))
-    if pos == []:
-        memo[target] = (None, None)
-    else:
-        memo[target] = min(map(lambda x: (x[0] + 1, x[1]), pos))
-    return memo[target]
-
 
 def find_fewest_coins(coins, target):
     if target < 0:
-        raise ValueError('Negative value given.')
+        raise ValueError('Impossible.')
+
+    # memo[X] = (n, c), where n is the min number of coins to sum X and c is the first of those coins
     memo = {0: (0, None)}
+    for i in range(target):
+        if i in memo:
+            for c in coins:
+                if i + c in memo:
+                    memo[i + c] = min(memo[i + c], (memo[i][0] + 1, c))
+                else:
+                    memo[i + c] = (memo[i][0] + 1, c)
+
+    if target not in memo:
+        raise ValueError('Impossible.')
+
     used_coins = []
     t = target
-    num, first = min_num_coins(coins, target, memo)
-    if num is None:
-        raise ValueError('Impossible combination.')
-    while first is not None:
+    while t != 0:
+        _, first = memo[t]
         used_coins.append(first)
         t -= first
-        _, first = memo[t]
     return used_coins
