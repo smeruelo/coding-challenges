26337d850c0a36c10825316586fe00e5b0286275
Sonia Meruelo <smeruelo@gmail.com>
2020-05-16 18:15:28 +0200

    exercism, python: hangman (no-FPR solution)

--- exercism/python/hangman/hangman.py
+++ exercism/python/hangman/hangman.py
@@ -1,5 +1,3 @@
-# Game status categories
-# Change the values as you see fit
 STATUS_WIN = "win"
 STATUS_LOSE = "lose"
 STATUS_ONGOING = "ongoing"
@@ -9,12 +7,25 @@ class Hangman:
     def __init__(self, word):
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
+        self.word = word
+        self.guessed = ['_'] * len(word)
 
     def guess(self, char):
-        pass
+        if self.status != STATUS_ONGOING:
+            raise ValueError('Game is over.')
+        if char not in self.word or char in self.guessed:
+            self.remaining_guesses -= 1
+            if self.remaining_guesses < 0:
+                self.status = STATUS_LOSE
+        else:
+            for i, c in enumerate(self.word):
+                if c == char:
+                    self.guessed[i] = char
+            if self.get_masked_word() == self.word:
+                self.status = STATUS_WIN
 
     def get_masked_word(self):
-        pass
+        return ''.join(self.guessed)
 
     def get_status(self):
-        pass
+        return self.status
