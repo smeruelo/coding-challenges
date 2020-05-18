906ed7471c1e04574757fde6a2a1e4ff597625df
Sonia Meruelo <smeruelo@gmail.com>
2020-05-19 01:02:44 +0200

    exercism, python: dominoes (use named tuples)

--- exercism/python/dominoes/dominoes.py
+++ exercism/python/dominoes/dominoes.py
@@ -5,23 +5,29 @@
 # Nodes are tuples (ID, stone)
 # IDs are necessary to use sets, since there can be duplicated stones.
 
+from collections import namedtuple
+
+Stone = namedtuple('Stone', 'left right')
+Node = namedtuple('Node', 'id stone')
+
 def adjacents(value, nodes):
-    return set(filter(lambda n: value in n[1], nodes))
+    return set(filter(lambda n: value in n.stone, nodes))
 
 def reverse(node):
-    return (node[0], (node[1][1], node[1][0]))
+    return Node(node.id, Stone(node.stone.right, node.stone.left))
 
 def can_chain(dominoes):
     def rec(chain, rest):
         if not rest:
-            return chain if chain[-1][1] == chain[0][0] else None
-        for n in adjacents(chain[-1][1], rest):
-            nxt = n if n[1][0] == chain[-1][1] else reverse(n)
-            if (result := rec(chain + [nxt[1]], rest - {n})) is not None:
+            return chain if chain[-1].right == chain[0].left else None
+        for n in adjacents(chain[-1].right, rest):
+            nxt = n if n.stone.left == chain[-1].right else reverse(n)
+            if (result := rec(chain + [nxt.stone], rest - {n})) is not None:
                 return result
         return None
 
     if dominoes == []:
         return []
-    nodes = set(zip(range(len(dominoes)), dominoes))
-    return rec([nodes.pop()[1]], nodes)
+    stones = map(lambda s: Stone(*s), dominoes)
+    nodes = set(map(lambda n: Node(*n), zip(range(len(dominoes)), stones)))
+    return rec([nodes.pop().stone], nodes)
