d4d94ad9b609487774d811bc45446c04c43bed74
Sonia Meruelo <smeruelo@gmail.com>
2020-05-20 00:45:43 +0200

    exercism, python: custom set (using lists)

--- exercism/python/custom-set/custom_set.py
+++ exercism/python/custom-set/custom_set.py
@@ -1,33 +1,60 @@
 class CustomSet:
+    """Simple implementation of a set, using lists to store the elements.
+    We could reduce the cost O(n * m) to O(max(n, m)) if we kept the elements ordered."""
+
+    # O(n log n)
     def __init__(self, elements=[]):
-        self._elements = set(elements)
+        if len(elements) == 0:
+            self._elements = []
+            return
+        sorted_elements = sorted(elements)
+        self._elements = [sorted_elements[0]]
+        prev = sorted_elements[0]
+        for e in sorted_elements:
+            if e != prev:
+                self._elements.append(e)
+            prev = e
 
+    @property
+    def elements(self):
+        return self._elements
+
+    # O(1)
     def isempty(self):
-        return len(self._elements) == 0
+        return len(self.elements) == 0
 
+    # O(n)
     def __contains__(self, element):
-        return element in self._elements
+        return element in self.elements
 
+    # O(n * m)
     def issubset(self, other):
-        return self._elements <= other._elements
+        return all(map(lambda e: e in other, self.elements))
 
+    # O(n * m)
     def isdisjoint(self, other):
-        return self._elements.isdisjoint(other._elements)
+        return self.intersection(other).isempty()
 
+    # O(n log n + m log m)
     def __eq__(self, other):
-        return self._elements == other._elements
+        return sorted(self.elements) == sorted(other.elements)
 
+    # O(n)
     def add(self, element):
-        self._elements.add(element)
+        if not element in self:
+            self.elements.append(element)
 
+    # O(n * m)
     def intersection(self, other):
-        return CustomSet(list(self._elements & other._elements))
+        return CustomSet([e for e in self.elements if e in other])
 
+    # O(n * m)
     def __sub__(self, other):
-        return CustomSet(list(self._elements - other._elements))
+        return CustomSet([e for e in self.elements if e not in other])
 
+    # O(m)
     def __add__(self, other):
-        return CustomSet(list(self._elements | other._elements))
+        return CustomSet(self.elements + other.elements)
 
     def __repr__(self):
-        return self._elements.__repr__()
+        return '{' + self.elements.__repr__()[1:-1] + '}'
