073e866001bedb199179b52b0ed41578d33b915a
Sonia Meruelo <smeruelo@gmail.com>
2020-05-19 11:32:10 +0200

    exercism, python: diffie-hellman (solution)

--- exercism/python/diffie-hellman/diffie_hellman.py
+++ exercism/python/diffie-hellman/diffie_hellman.py
@@ -1,10 +1,15 @@
+# https://exercism.io/my/solutions/4baf13522ca34b88928afff624d07cc1
+
+from secrets import choice
+
+
 def private_key(p):
-    pass
+    return choice(range(2, p))
 
 
 def public_key(p, g, private):
-    pass
+    return (g ** private) % p
 
 
 def secret(p, public, private):
-    pass
+    return (public ** private) % p
