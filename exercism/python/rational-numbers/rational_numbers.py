d17eb552f4ed8ed01ba4d90600fd5032e7e698a8
Sonia Meruelo <smeruelo@gmail.com>
2020-05-19 23:32:04 +0200

    exercism, python: rational numbers (solution)

--- exercism/python/rational-numbers/rational_numbers.py
+++ exercism/python/rational-numbers/rational_numbers.py
@@ -1,34 +1,38 @@
-from __future__ import division
+# https://exercism.io/my/solutions/96ff5b3cd1a74c3eb920d7f523fa0912
+
+from math import gcd, log
 
 
 class Rational:
     def __init__(self, numer, denom):
-        self.numer = None
-        self.denom = None
+        sign = -1 if numer * denom < 0 else 1
+        g = gcd(numer, denom)
+        self.numer = sign * abs(numer) // g
+        self.denom = abs(denom) // g
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
 
     def __repr__(self):
-        return '{}/{}'.format(self.numer, self.denom)
+        return f'{self.numer}/{self.denom}'
 
     def __add__(self, other):
-        pass
+        return Rational(self.numer * other.denom + other.numer * self.denom, self.denom * other.denom)
 
     def __sub__(self, other):
-        pass
+        return Rational(self.numer * other.denom - other.numer * self.denom, self.denom * other.denom)
 
     def __mul__(self, other):
-        pass
+        return Rational(self.numer * other.numer, self.denom * other.denom)
 
     def __truediv__(self, other):
-        pass
+        return Rational(self.numer * other.denom, self.denom * other.numer)
 
     def __abs__(self):
-        pass
+        return Rational(abs(self.numer), abs(self.denom))
 
     def __pow__(self, power):
-        pass
+        return Rational(pow(self.numer, abs(power)), pow(self.denom, abs(power)))
 
     def __rpow__(self, base):
-        pass
+        return pow(2, log(base ** self.numer, 2) / self.denom)
