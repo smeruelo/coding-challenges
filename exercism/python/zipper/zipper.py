2a92d48357263c89f800b976376d259a66c82069
Sonia Meruelo <smeruelo@gmail.com>
2020-05-23 23:57:37 +0200

    exercism, python: zipper (solution, using mutable data)
    Also, not efficient

--- exercism/python/zipper/zipper.py
+++ exercism/python/zipper/zipper.py
@@ -12,48 +12,32 @@ class Zipper:
         return Zipper(deepcopy(tree))
 
     def value(self):
-        if self._focus == None:
-            raise Exception("nope")
         return self._focus['value']
 
     def set_value(self, value):
-        if self._focus == None:
-            raise Exception("nope")
-            # tree = {"value": value, "left": None, "right": None}
-            # last, parent = self._zipper[-1]
-            # if last == 'left':
-        else:
-            tree = deepcopy(self._focus)
-            tree['value'] = value
-        return Zipper(tree, self._zipper)
+        tree = deepcopy(self._focus)
+        tree['value'] = value
+        return Zipper(tree, deepcopy(self._zipper))
 
     def left(self):
-        if self._focus == None:
-            raise Exception("nope")
         if self._focus['left'] == None:
             return None
         return Zipper(self._focus["left"], self._zipper + [("left", self._focus)])
 
     def set_left(self, branch):
-        if self._focus == None:
-            raise Exception("nope")
         tree = deepcopy(self._focus)
         tree['left'] = branch
-        return Zipper(tree, self._zipper)
+        return Zipper(tree, deepcopy(self._zipper))
 
     def right(self):
-        if self._focus == None:
-            raise Exception("nope")
         if self._focus['right'] == None:
             return None
         return Zipper(self._focus["right"], self._zipper + [("right", self._focus)])
 
     def set_right(self, branch):
-        if self._focus == None:
-            raise Exception("nope")
         tree = deepcopy(self._focus)
         tree['right'] = branch
-        return Zipper(tree, self._zipper)
+        return Zipper(tree, deepcopy(self._zipper))
 
     def up(self):
         if self._zipper == []:
@@ -62,13 +46,16 @@ class Zipper:
         return Zipper(parent, self._zipper[:-1])
 
     def to_tree(self):
-        def rec(tree, zipper):
+        def rec(tree, move, zipper):
             if zipper == []:
-                tree[move]
-                return tree
-            move, parent = self._zipper[0]
+                tree[move] = self._focus
+                return
+            nxt_move, parent = zipper[0]
+            rec(parent, nxt_move, zipper[1:])
 
-        tree = dict()
+        if self._zipper == []:
+            return self._focus
         move, parent = self._zipper[0]
-        rec(parent, move, self._zipper[1:])
+        tree = dict(parent)
+        rec(tree, move, self._zipper[1:])
         return tree
