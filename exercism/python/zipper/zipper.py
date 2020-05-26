c9acd749a7638c1cc9f20d52371b5a5429501ad3
Sonia Meruelo <smeruelo@gmail.com>
2020-05-27 00:57:08 +0200

    exercism, python: zipper (improvements)

--- exercism/python/zipper/zipper.py
+++ exercism/python/zipper/zipper.py
@@ -1,8 +1,5 @@
 # https://exercism.io/my/solutions/1a1c96a4fa304f4aa9f72b99a9cf8411
 
-from copy import deepcopy
-
-
 LEFT = "left"
 RIGHT = "right"
 VALUE = "value"
@@ -32,7 +29,7 @@ class Zipper:
         return self._value
 
     def set_value(self, value):
-        return Zipper(value, self._left, self._right, deepcopy(self._zipper))
+        return Zipper(value, self._left, self._right, self._zipper[:])
 
     def left(self):
         if self._left is None:
@@ -45,7 +42,7 @@ class Zipper:
         )
 
     def set_left(self, branch):
-        return Zipper(self.value(), Zipper.from_tree(branch), self._right, deepcopy(self._zipper))
+        return Zipper(self.value(), Zipper.from_tree(branch), self._right, self._zipper[:])
 
     def right(self):
         if self._right is None:
@@ -58,7 +55,7 @@ class Zipper:
         )
 
     def set_right(self, branch):
-        return Zipper(self.value(), self._left, Zipper.from_tree(branch), deepcopy(self._zipper))
+        return Zipper(self.value(), self._left, Zipper.from_tree(branch), self._zipper[:])
 
     def up(self):
         if self._zipper == []:
@@ -66,30 +63,30 @@ class Zipper:
         _, parent = self._zipper[-1]
         return Zipper(parent.value(), parent._left, parent._right, self._zipper[:-1])
 
-    def branch(self, choice):
+    def _branch(self, choice):
         return self._left if choice == LEFT else self._right
 
-    def current_to_bottom_tree(self):
-        left_branch = None if self._left is None else self._left.current_to_bottom_tree()
-        right_branch = None if self._right is None else self._right.current_to_bottom_tree()
+    def _current_to_bottom_tree(self):
+        left_branch = None if self._left is None else self._left._current_to_bottom_tree()
+        right_branch = None if self._right is None else self._right._current_to_bottom_tree()
         return {"value": self.value(), LEFT: left_branch, RIGHT: right_branch}
 
     def to_tree(self):
-        down = self.current_to_bottom_tree()
+        down = self._current_to_bottom_tree()
         if self._zipper == []:
             return down
 
         choice, parent = self._zipper[-1]
-        if parent.branch(OPPOSITE[choice]) is None:
+        if parent._branch(OPPOSITE[choice]) is None:
             other_branch = None
         else:
-            other_branch = parent.branch(OPPOSITE[choice]).current_to_bottom_tree()
+            other_branch = parent._branch(OPPOSITE[choice])._current_to_bottom_tree()
         tree = {"value": parent.value(), choice: down, OPPOSITE[choice]: other_branch}
 
         for choice, parent in reversed(self._zipper[:-1]):
-            if parent.branch(OPPOSITE[choice]) is None:
+            if parent._branch(OPPOSITE[choice]) is None:
                 other_branch = None
             else:
-                other_branch = parent.branch(OPPOSITE[choice]).current_to_bottom_tree()
+                other_branch = parent._branch(OPPOSITE[choice])._current_to_bottom_tree()
             tree = {"value": parent.value(), choice: tree, OPPOSITE[choice]: other_branch}
         return tree
