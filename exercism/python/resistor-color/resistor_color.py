8b19222c4dab8e7aa67fa292792ab42710ff971c
Sonia Meruelo <smeruelo@gmail.com>
2020-05-27 00:52:45 +0200

    exercism, python: resistor color (solution)

--- exercism/python/resistor-color/resistor_color.py
+++ exercism/python/resistor-color/resistor_color.py
@@ -1,6 +1,17 @@
 def color_code(color):
-    pass
+    return colors().index(color)
 
 
 def colors():
-    pass
+    return [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    ]
