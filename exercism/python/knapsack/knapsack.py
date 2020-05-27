bee820f9ad92642b68b4b1ae9807327160c2acce
Sonia Meruelo <smeruelo@gmail.com>
2020-05-27 23:07:26 +0200

    exercism, python: knapsack (iterative solution)

--- exercism/python/knapsack/knapsack.py
+++ exercism/python/knapsack/knapsack.py
@@ -1,27 +1,22 @@
 # https://exercism.io/my/solutions/09770b6fe3e04b8bb2070008e26f194d
 
-# Recursive approach
-# Using memoization, we reduce the number of recursive calls from 2^n, to (n+1)(max_weight+1)
+# Iterative approach.
+# In a recursive approach, we used memoization to reduce the number of recursive calls
+# from 2^n, to (n+1)(max_weight+1)
 # memo[i][x] = max total value adding the first i items, considering a max capacity of x
+# Knowing this, we can directly build this matrix, in a bottom-up way
 
 
 def maximum_value(maximum_weight, items):
+    n = len(items)
+    memo = [[0] * (maximum_weight + 1) for _ in range(n + 1)]
 
-    def rec(i, x):
-        if memo[i][x] is not None:
-            return memo[i][x]
-
-        if i == 0:
-            memo[i][x] = 0
-            return 0
-
+    for i in range(1, n + 1):
         weight = items[i - 1]["weight"]
         value = items[i - 1]["value"]
-        omiting_item = rec(i - 1, x)
-        including_item = value + rec(i - 1, x - weight) if x - weight >= 0 else 0
-        memo[i][x] = max(omiting_item, including_item)
-        return memo[i][x]
+        for x in range(maximum_weight + 1):
+            omiting_item = memo[i - 1][x]
+            including_item = value + memo[i - 1][x - weight] if x - weight >= 0 else 0
+            memo[i][x] = max(omiting_item, including_item)
 
-    n = len(items)
-    memo = [[None] * (maximum_weight + 1) for _ in range(n + 1)]
-    return rec(n, maximum_weight)
+    return memo[n][maximum_weight]
