2933bdbcb6fa3b47d19ed2dd0288afb0f6b71101
Sonia Meruelo <smeruelo@gmail.com>
2020-06-03 23:11:46 +0200

    exercism, python: sgf parsing (solution)

--- exercism/python/sgf-parsing/sgf_parsing.py
+++ exercism/python/sgf-parsing/sgf_parsing.py
@@ -1,3 +1,6 @@
+from string import ascii_uppercase
+
+
 class SgfTree:
     def __init__(self, properties=None, children=None):
         self.properties = properties or {}
@@ -25,5 +28,73 @@ class SgfTree:
         return not self == other
 
 
-def parse(input_string):
-    pass
+def parse_prop_name(s, i):
+    name = ""
+    while i < len(s) and s[i] != "[":
+        if s[i] in "();]" or s[i] not in ascii_uppercase:
+            raise ValueError("Syntax Error.")
+        name += s[i]
+        i += 1
+    if i == len(s):
+        raise ValueError("Syntax Error.")
+    return name, i
+
+
+def parse_prop_value(s, i):
+    if s[i] != "[":
+        raise ValueError("Syntax Error.")
+    i += 1
+    value = ""
+    while i < len(s) and s[i] != "]":
+        if s[i] == "\\" and i + 1 < len(s):
+            i += 1
+        elif s[i] in "();[":
+            raise ValueError("Syntax Error.")
+        value += s[i]
+        i += 1
+    if i == len(s):
+        raise ValueError("Syntax Error.")
+    return value.replace("\t", " "), i+1
+
+
+def parse_node(s, i):
+    if s[i] != ";":
+        raise ValueError("Syntax Error.")
+    i += 1
+    properties = dict()
+    while i < len(s) and s[i] not in ";()":
+        name, i = parse_prop_name(s, i)
+        values = []
+        while s[i] == "[":
+            value, i = parse_prop_value(s, i)
+            values.append(value)
+        properties[name] = values
+    if i == len(s):
+        raise ValueError("Syntax Error.")
+    if s[i] == ";":
+        child, i = parse_node(s, i)
+        return SgfTree(properties, [child]), i
+    if s[i] == "(":
+        left_child, i = parse_tree(s, i)
+        right_child, i = parse_tree(s, i)
+        return SgfTree(properties, [left_child, right_child]), i
+    if s[i] == ")":
+        return SgfTree(properties), i
+
+
+def parse_tree(s ,i):
+    if s[i] != "(":
+        raise ValueError("Syntax Error")
+    tree, i = parse_node(s, i+1)
+    if s[i] != ")":
+        raise ValueError("Syntax Error")
+    return tree, i+1
+
+
+def parse(s):
+    if len(s) == 0:
+        raise ValueError("Syntax Error")
+    tree, i = parse_tree(s, 0)
+    if i != len(s):
+        raise ValueError("Syntax Error")
+    return tree
