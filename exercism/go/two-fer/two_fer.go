401310431eba3162acb082101e901be4508336ce
Sonia Meruelo <smeruelo@gmail.com>
2020-06-26 00:35:37 +0200

    exercism, go: two fer (solution)

--- exercism/go/two-fer/two_fer.go
+++ exercism/go/two-fer/two_fer.go
@@ -1,15 +1,12 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
+// https://exercism.io/my/solutions/1a5af5ece75b49d48733fb0cd439bdf5
 
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package twofer
 
-// ShareWith should have a comment documenting it.
+import "fmt"
+
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	if name == "" {
+		name = "you"
+	}
+	return fmt.Sprintf("One for %v, one for me.", name)
 }
