28b5e9f68649a46171dd11720d85ea8ecc393eea
Sonia Meruelo <smeruelo@gmail.com>
2020-06-26 23:57:25 +0200

    exercism, go: two fer (mentor's feedback)

--- exercism/go/two-fer/two_fer.go
+++ exercism/go/two-fer/two_fer.go
@@ -1,12 +1,14 @@
 // https://exercism.io/my/solutions/1a5af5ece75b49d48733fb0cd439bdf5
 
+// Package twofer provides awesome friendship tools :)
 package twofer
 
 import "fmt"
 
+// ShareWith returns a generous message.
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
 	}
-	return fmt.Sprintf("One for %v, one for me.", name)
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
