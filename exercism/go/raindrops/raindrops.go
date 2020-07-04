8dbaa7bf29e6ac4245c941a389b80ec31d3835e6
Sonia Meruelo <smeruelo@gmail.com>
2020-07-05 01:53:19 +0200

    exercism, go: raindrops (multiple ifs)

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -1,1 +1,20 @@
 package raindrops
+
+import "strconv"
+
+func Convert(number int) string {
+	var drops = ""
+	if number%3 == 0 {
+		drops += "Pling"
+	}
+	if number%5 == 0 {
+		drops += "Plang"
+	}
+	if number%7 == 0 {
+		drops += "Plong"
+	}
+	if drops == "" {
+		return strconv.Itoa(number)
+	}
+	return drops
+}
