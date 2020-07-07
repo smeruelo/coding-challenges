481af1b02364f7e6cd770eadbd81058266704e46
Sonia Meruelo <smeruelo@gmail.com>
2020-07-08 00:21:29 +0200

    exercism, go: rna transcription (solution)

--- exercism/go/rna-transcription/rna_transcription.go
+++ exercism/go/rna-transcription/rna_transcription.go
@@ -1,4 +1,17 @@
+// https://exercism.io/my/solutions/e19201440e64447b8467d42ae88bf21f
+
 package strand
 
+import "strings"
+
+var complement = map[rune]rune{
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+}
+
+// ToRNA transforms a DNA string into an RNA one
 func ToRNA(dna string) string {
+	return strings.Map(func(nucleotide rune) rune { return complement[nucleotide] }, dna)
 }
