e56ea0959cab8aadd2d4c2c97d381be6e4019219
Sonia Meruelo <smeruelo@gmail.com>
2020-07-07 16:02:59 +0200

    exercism, go: proverb (solution)

--- exercism/go/proverb/proverb.go
+++ exercism/go/proverb/proverb.go
@@ -1,15 +1,16 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package proverb
+
+import "fmt"
 
 // Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+	length := len(rhyme)
+	output := make([]string, length)
+	if length > 0 {
+		for i := 1; i < length; i++ {
+			output[i-1] = fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i-1], rhyme[i])
+		}
+		output[length-1] = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
+	}
+	return output
 }
