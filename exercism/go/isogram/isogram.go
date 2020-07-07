880c014d99358b4c5a048e82369f77a0fb4bb434
Sonia Meruelo <smeruelo@gmail.com>
2020-07-08 00:07:29 +0200

    exercism, go: isogram (solution)

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -1,1 +1,21 @@
+// https://exercism.io/my/solutions/747fe2ed9a704505a22508d80525ef25
 package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+// IsIsogram determines whether a text is an isogram or not
+func IsIsogram(text string) bool {
+	appearsInText := make(map[rune]bool)
+	for _, c := range strings.ToLower(text) {
+		if unicode.IsLetter(c) {
+			if appearsInText[c] {
+				return false
+			}
+			appearsInText[c] = true
+		}
+	}
+	return true
+}
