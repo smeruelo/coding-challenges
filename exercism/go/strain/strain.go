564e03bf4d95f3e3c3f3db4775eaba9baa9fac53
Sonia Meruelo <smeruelo@gmail.com>
2020-07-09 01:02:34 +0200

    exercism, go: strain (improvements)

--- exercism/go/strain/strain.go
+++ exercism/go/strain/strain.go
@@ -1,3 +1,5 @@
+// https://exercism.io/my/solutions/8f3f848a06654a8a84e0237e4b69b69c
+
 package strain
 
 // Ints represents an array of integers
@@ -10,61 +12,36 @@ type Lists [][]int
 type Strings []string
 
 // Keep receives a predicate and filters out the elements for which that predicate is false
-func (in Ints) Keep(f func(int) bool) Ints {
-	if in == nil {
-		return nil
-	}
-
-	out := Ints{}
+func (in Ints) Keep(f func(int) bool) (out Ints) {
 	for _, n := range in {
 		if f(n) {
 			out = append(out, n)
 		}
 	}
-	return out
+	return
 }
 
 // Discard receives a predicate and filters out the elements for which that predicate is true
 func (in Ints) Discard(f func(int) bool) Ints {
-	if in == nil {
-		return nil
-	}
-
-	out := Ints{}
-	for _, n := range in {
-		if !f(n) {
-			out = append(out, n)
-		}
-	}
-	return out
+	return in.Keep(func(n int) bool { return !f(n) })
 }
 
 // Keep receives a predicate and filters out the elements for which that predicate is false
-func (in Lists) Keep(f func([]int) bool) Lists {
-	if in == nil {
-		return nil
-	}
-
-	out := Lists{}
+func (in Lists) Keep(f func([]int) bool) (out Lists) {
 	for _, l := range in {
 		if f(l) {
 			out = append(out, l)
 		}
 	}
-	return out
+	return
 }
 
 // Keep receives a predicate and filters out the elements for which that predicate is false
-func (in Strings) Keep(f func(string) bool) Strings {
-	if in == nil {
-		return nil
-	}
-
-	out := Strings{}
+func (in Strings) Keep(f func(string) bool) (out Strings) {
 	for _, s := range in {
 		if f(s) {
 			out = append(out, s)
 		}
 	}
-	return out
+	return
 }
