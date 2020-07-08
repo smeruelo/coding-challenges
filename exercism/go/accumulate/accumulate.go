a4952ab8675ee376228a86ce87b0b361cb7e7d4f
Sonia Meruelo <smeruelo@gmail.com>
2020-07-08 11:52:47 +0200

    exercism, go: accumulate (solution)

--- exercism/go/accumulate/accumulate.go
+++ exercism/go/accumulate/accumulate.go
@@ -1,1 +1,14 @@
+// https://exercism.io/my/solutions/753d4bb58be34c148925b28656f5e3f9
+
 package accumulate
+
+type stringConverter func(s string) string
+
+// Accumulate applies the given function to each of the strings in an array.
+func Accumulate(collection []string, f stringConverter) []string {
+	result := make([]string, len(collection))
+	for i, s := range collection {
+		result[i] = f(s)
+	}
+	return result
+}
