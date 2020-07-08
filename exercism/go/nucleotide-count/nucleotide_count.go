378b1dec73d4d7d8836b19ea57e124fb56488c64
Sonia Meruelo <smeruelo@gmail.com>
2020-07-08 11:05:44 +0200

    exercism, go: nucleotide count (solution)

--- exercism/go/nucleotide-count/nucleotide_count.go
+++ exercism/go/nucleotide-count/nucleotide_count.go
@@ -1,19 +1,38 @@
 package dna
 
-// Histogram is a mapping from nucleotide to its count in given DNA.
-// Choose a suitable data type.
-type Histogram
+import (
+	"errors"
+)
 
-// DNA is a list of nucleotides. Choose a suitable data type.
-type DNA
+type nucleotide rune
+
+const (
+	A nucleotide = 'A'
+	C nucleotide = 'C'
+	G nucleotide = 'G'
+	T nucleotide = 'T'
+)
+
+// Histogram is a mapping from nucleotide to its count in given DNA.
+type Histogram map[nucleotide]int
+
+// DNA is a list of nucleotides.
+type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
-///
-// Counts is a method on the DNA type. A method is a function with a special receiver argument.
-// The receiver appears in its own argument list between the func keyword and the method name.
-// Here, the Counts method has a receiver of type DNA named d.
-func (d DNA) Counts() (Histogram, error) {
-	var h Histogram
-	return h, nil
+func (strand DNA) Counts() (Histogram, error) {
+	var hist Histogram = Histogram{A: 0, C: 0, G: 0, T: 0}
+	var err error
+
+	for _, c := range strand {
+		n := nucleotide(c)
+		_, ok := hist[n]
+		if ok {
+			hist[n] += 1
+		} else {
+			err = errors.New("invalid DNA strand.")
+		}
+	}
+	return hist, err
 }
