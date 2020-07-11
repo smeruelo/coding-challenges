5f95a54a9a0eab6fc71742ee0d261ff758a2066d
Sonia Meruelo <smeruelo@gmail.com>
2020-07-11 20:01:09 +0200

    exercism, go: difference of squares (solution)

--- exercism/go/difference-of-squares/difference_of_squares.go
+++ exercism/go/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,20 @@
+// https://exercism.io/my/solutions/d06121fc761d49e9bd08c16a820b94e1
+
 package diffsquares
+
+// SquareOfSum returns the square of the sum of the first n natural numbers.
+func SquareOfSum(n int) int {
+	sum := n * (n + 1) / 2
+	return sum * sum
+}
+
+// SumOfSquares returns the sum of the squared first n natural numbers.
+func SumOfSquares(n int) int {
+	return n * (n + 1) * (2*n + 1) / 6
+}
+
+// Difference returns the square of the sum of the first n natural numbers
+// minus the sum of the squared first n natural numbers.
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
