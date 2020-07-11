a07375d83c3c42dfad81bf2972f3ab350890398f
Sonia Meruelo <smeruelo@gmail.com>
2020-07-11 11:33:17 +0200

    exercism, go: isogram (mentor's feedback)
    Lower characters only when needed

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -1,16 +1,17 @@
 // https://exercism.io/my/solutions/747fe2ed9a704505a22508d80525ef25
+
 package isogram
 
 import (
-	"strings"
 	"unicode"
 )
 
 // IsIsogram determines whether a text is an isogram or not
 func IsIsogram(text string) bool {
 	appearsInText := make(map[rune]bool)
-	for _, c := range strings.ToLower(text) {
+	for _, c := range text {
 		if unicode.IsLetter(c) {
+			c = unicode.ToLower(c)
 			if appearsInText[c] {
 				return false
 			}
