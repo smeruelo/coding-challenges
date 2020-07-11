117ca4fa1c437f67e0e9ff4ac98fb7be3183a4ac
Sonia Meruelo <smeruelo@gmail.com>
2020-07-12 00:25:36 +0200

    exercism, go: ETL (solution)

--- exercism/go/etl/etl.go
+++ exercism/go/etl/etl.go
@@ -1,1 +1,17 @@
+// https://exercism.io/my/solutions/e783f2c3fde94fba9096cad8df62dd66
+
 package etl
+
+import "strings"
+
+// Transform takes a translation table in the form points->(letter, letter, ..)
+// and returns a new one in the form letter->points
+func Transform(pointsOld map[int][]string) map[string]int {
+	pointsNew := make(map[string]int)
+	for k, v := range pointsOld {
+		for _, letter := range v {
+			pointsNew[strings.ToLower(letter)] = k
+		}
+	}
+	return pointsNew
+}
