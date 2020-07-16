97f77a4b0463fe8121fa00d3e379f874fb8cea6f
Sonia Meruelo <smeruelo@gmail.com>
2020-07-16 11:56:36 +0200

    exercism, go: clock (mentor's feedback)

--- exercism/go/clock/clock.go
+++ exercism/go/clock/clock.go
@@ -4,39 +4,31 @@ package clock
 
 import "fmt"
 
-const (
-	minutesInADay   = 1440
-	minutesInAnHour = 60
-)
-
-// modulo returns the unsigned reminder of x/y
-func modulo(x, y int) int {
-	return ((x % y) + y) % y
-}
-
-// Clock represents a time in the day, using hours (h in [0, 23]) and minutes (m in [0, 59])
+// Clock represents a time in the day, in minutes (m in [0, 1440))
 type Clock struct {
-	h int
 	m int
 }
 
 // New takes an arbitrarly big (or even negative) number of hours and minutes and returns a Clock
 func New(hours, minutes int) Clock {
-	minuteOfDay := modulo(hours*minutesInAnHour+minutes, minutesInADay)
-	return Clock{h: minuteOfDay / minutesInAnHour, m: minuteOfDay % minutesInAnHour}
+	m := (hours*60 + minutes) % (24 * 60)
+	if m < 0 {
+		m += 24 * 60
+	}
+	return Clock{m: m}
 }
 
 // Add sums the given number of minutes to a Clock and returns a new one
 func (c Clock) Add(minutes int) Clock {
-	return New(c.h, c.m+minutes)
+	return New(0, c.m+minutes)
 }
 
 // Subtract deducts the given number of minutes from a Clock and returns a new one
 func (c Clock) Subtract(minutes int) Clock {
-	return New(c.h, c.m-minutes)
+	return New(0, c.m-minutes)
 }
 
 // String returns a string representation of a Clock, in the form hh:mm
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.h, c.m)
+	return fmt.Sprintf("%02d:%02d", c.m/60, c.m%60)
 }
