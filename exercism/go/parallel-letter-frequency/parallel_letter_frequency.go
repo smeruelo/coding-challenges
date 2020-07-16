1216e314096c189b5426a9ac6100cf47b2692b9c
Sonia Meruelo <smeruelo@gmail.com>
2020-07-16 22:33:01 +0200

    exercism, go: parallel letter frequency (solution, channel)

--- exercism/go/parallel-letter-frequency/parallel_letter_frequency.go
+++ exercism/go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -2,8 +2,6 @@
 
 package letter
 
-import "sync"
-
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -17,31 +15,23 @@ func Frequency(s string) FreqMap {
 	return m
 }
 
-// accFrequency counts the frecuency of each rune in a given text and
-// accumulates it in an existing FreqMap, which can be accessed concurrently.
-func accFrequency(s string, totalCount FreqMap, mux *sync.Mutex, wg *sync.WaitGroup) {
-	defer wg.Done()
-	count := Frequency(s)
-
-	mux.Lock()
-	for k, v := range count {
-		totalCount[k] += v
-	}
-	mux.Unlock()
-}
-
-// ConcurrentFrequency counts the frecuency of each rune in a series of texts.
-// Texts are analyzed concurrently.
+// ConcurrentFrequency counts the frecuency of each rune in a series of texts,
+// which are analyzed concurrently.
 func ConcurrentFrequency(texts []string) FreqMap {
-	totalCount := make(FreqMap)
-	var mux sync.Mutex
-	var wg sync.WaitGroup
+	count := make(FreqMap)
+	ch := make(chan FreqMap)
 
-	for _, s := range texts {
-		wg.Add(1)
-		go accFrequency(s, totalCount, &mux, &wg)
+	for _, text := range texts {
+		go func(s string) {
+			ch <- Frequency(s)
+		}(text)
 	}
-	wg.Wait()
 
-	return totalCount
+	for i := 0; i < len(texts); i++ {
+		for k, v := range <-ch {
+			count[k] += v
+		}
+	}
+
+	return count
 }
