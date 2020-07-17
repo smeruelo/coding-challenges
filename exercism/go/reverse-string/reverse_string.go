ef35006022847583956fad5591bed7f5adb3636f
Sonia Meruelo <smeruelo@gmail.com>
2020-07-17 11:09:21 +0200

    exercism, go: reverse string (solution, inefficient)

--- exercism/go/reverse-string/reverse_string.go
+++ exercism/go/reverse-string/reverse_string.go
@@ -1,1 +1,12 @@
+// https://exercism.io/my/solutions/856cf8f8a5984c8fabaef8542873f2ab
+
 package reverse
+
+// Reverse returns a reversed copy of the given string
+func Reverse(s string) string {
+	rev := ""
+	for _, char := range s {
+		rev = string(char) + rev
+	}
+	return rev
+}
