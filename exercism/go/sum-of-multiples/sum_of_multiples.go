fadb8daf806ed3e5f015a99b10f6f5950540d852
Sonia Meruelo <smeruelo@gmail.com>
2020-08-05 00:33:12 +0200

    exercism, go: sum of multiples (solution)

--- exercism/go/sum-of-multiples/sum_of_multiples.go
+++ exercism/go/sum-of-multiples/sum_of_multiples.go
@@ -1,1 +1,30 @@
+// https://exercism.io/my/solutions/6db367aef2cd49aebd983f04e23a7585
+
 package summultiples
+
+func isMultiple(a, b int) bool {
+	if b == 0 {
+		return false
+	}
+	return a%b == 0
+}
+
+func isMultipleOfAny(n int, divisors []int) bool {
+	for _, d := range divisors {
+		if isMultiple(n, d) {
+			return true
+		}
+	}
+	return false
+}
+
+// SumMultiples calculates the sum of numbers < limit that are multiples of any of given divisors
+func SumMultiples(limit int, divisors ...int) int {
+	sum := 0
+	for i := 1; i < limit; i++ {
+		if isMultipleOfAny(i, divisors) {
+			sum += i
+		}
+	}
+	return sum
+}
