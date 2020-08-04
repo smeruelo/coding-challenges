aade08e5cc96e247a9abede294a6bf745e40a33f
Sonia Meruelo <smeruelo@gmail.com>
2020-08-05 00:02:28 +0200

    exercism, go: tournament(mentor's feedback)

--- exercism/go/tournament/tournament.go
+++ exercism/go/tournament/tournament.go
@@ -11,21 +11,9 @@ import (
 	"strings"
 )
 
-type matchResult string
-
-const (
-	draw matchResult = "draw"
-	loss matchResult = "loss"
-	win  matchResult = "win"
-)
-
-var (
-	points         = map[matchResult]int{draw: 1, loss: 0, win: 3}
-	oppositeResult = map[matchResult]matchResult{draw: draw, loss: win, win: loss}
-)
-
 // team stores a team's matches' results
 type team struct {
+	name    string
 	matches int
 	draws   int
 	losses  int
@@ -46,53 +34,49 @@ func readResults(r io.Reader) (map[string]team, error) {
 		if len(matchInfo) != 3 {
 			return teams, errors.New("invalid input")
 		}
-		nameTeam1, nameTeam2, r := matchInfo[0], matchInfo[1], matchInfo[2]
-		result := matchResult(r)
+		nameTeam1, nameTeam2, result := matchInfo[0], matchInfo[1], matchInfo[2]
 		t1, t2 := teams[nameTeam1], teams[nameTeam2]
 
 		switch result {
-		case draw:
+		case "draw":
 			t1.draws++
 			t2.draws++
-		case loss:
+			t1.points++
+			t2.points++
+		case "loss":
 			t1.losses++
 			t2.wins++
-		case win:
+			t2.points += 3
+		case "win":
 			t1.wins++
 			t2.losses++
+			t1.points += 3
 		default:
 			return teams, errors.New("invalid input")
 		}
 		t1.matches++
 		t2.matches++
-		t1.points += points[result]
-		t2.points += points[oppositeResult[result]]
+		t1.name = nameTeam1
+		t2.name = nameTeam2
 		teams[nameTeam1], teams[nameTeam2] = t1, t2
 	}
 
 	return teams, nil
 }
 
-// sortTeams provides a slice of team names, sorted by points and name
-func sortTeams(teams map[string]team) []string {
-	names := make([]string, len(teams))
-	i := 0
-	for k := range teams {
-		names[i] = k
-		i++
+// sortTeams provides a slice of teams, sorted by points and name
+func sortTeams(teams map[string]team) []team {
+	s := make([]team, 0, len(teams))
+	for _, t := range teams {
+		s = append(s, t)
 	}
-	sort.Slice(names, func(i, j int) bool {
-		pointsI := teams[names[i]].points
-		pointsJ := teams[names[j]].points
-		if pointsI > pointsJ {
-			return true
+	sort.Slice(s, func(i, j int) bool {
+		if s[i].points == s[j].points {
+			return s[i].name < s[j].name
 		}
-		if pointsI == pointsJ {
-			return names[i] < names[j]
-		}
-		return false
+		return s[i].points > s[j].points
 	})
-	return names
+	return s
 }
 
 // Tally reads a series of matches results and pretty prints them
@@ -106,17 +90,17 @@ func Tally(r io.Reader, w io.Writer) error {
 	header := "Team                           | MP |  W |  D |  L |  P\n"
 	bw.WriteString(header)
 
-	teamRow := func(team string) string {
-		results := teams[team]
-		mp := results.matches
-		w := results.wins
-		d := results.draws
-		l := results.losses
-		p := results.points
-		return fmt.Sprintf("%-30s | %2d | %2d | %2d | %2d | %2d\n", team, mp, w, d, l, p)
+	teamRow := func(t team) string {
+		name := t.name
+		mp := t.matches
+		w := t.wins
+		d := t.draws
+		l := t.losses
+		p := t.points
+		return fmt.Sprintf("%-30s | %2d | %2d | %2d | %2d | %2d\n", name, mp, w, d, l, p)
 	}
-	for _, team := range sortTeams(teams) {
-		bw.WriteString(teamRow(team))
+	for _, t := range sortTeams(teams) {
+		bw.WriteString(teamRow(t))
 	}
 
 	bw.Flush()
