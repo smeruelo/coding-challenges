94c04b9bf9b71cce92ec6aa669cbbe1657cc068c
Sonia Meruelo <smeruelo@gmail.com>
2020-08-05 23:56:41 +0200

    exercism, go: largest series product (naive solution)

--- exercism/go/largest-series-product/largest_series_product.go
+++ exercism/go/largest-series-product/largest_series_product.go
@@ -1,1 +1,31 @@
+// https://exercism.io/my/solutions/37c4f904c17442ebb83c3f85e1c1879b
+
 package lsproduct
+
+import (
+	"fmt"
+)
+
+// LargestSeriesProduct returns the largest product for a contiguous substring of length n.
+func LargestSeriesProduct(digits string, span int) (int, error) {
+	runes := []rune(digits)
+	if span < 0 || span > len(runes) {
+		return 0, fmt.Errorf("invalid span %q (it must be >0 and <= input's length)", span)
+	}
+
+	maxProduct := 0
+	for i := 0; i <= len(digits)-span; i++ {
+		product := 1
+		for j := 0; j < span; j++ {
+			d := int(runes[i+j] - '0')
+			if d < 0 || d > 9 {
+				return 0, fmt.Errorf("invalid input %q (it must contain only digits)", digits)
+			}
+			product *= d
+		}
+		if product > maxProduct {
+			maxProduct = product
+		}
+	}
+	return maxProduct, nil
+}
