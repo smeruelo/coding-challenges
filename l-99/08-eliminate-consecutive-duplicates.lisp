1c7c2cc921f68357d4f460415bc4e3c464b28d95
Sonia Meruelo <smeruelo@gmail.com>
2020-08-07 01:40:49 +0200

    l-99, use '() instead of nil for empty lists

--- l-99/08-eliminate-consecutive-duplicates.lisp
+++ l-99/08-eliminate-consecutive-duplicates.lisp
@@ -1,7 +1,7 @@
 ;; no tail-recursive
 (defun no-consecutive-dups (lst)
   (if (null lst)
-      nil
+      '()
       (if (eql (car lst) (cadr lst))
           (no-consecutive-dups (cdr lst))
           (cons (car lst) (no-consecutive-dups (cdr lst))))))
