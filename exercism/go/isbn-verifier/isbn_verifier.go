ee0295bcb6a1d00adb4e2d01fa81e107bec9374d
Sonia Meruelo <smeruelo@gmail.com>
2020-08-06 11:57:00 +0200

    exercism, go: isbn verifier (solution)

--- exercism/go/isbn-verifier/isbn_verifier.go
+++ exercism/go/isbn-verifier/isbn_verifier.go
@@ -1,1 +1,41 @@
 package isbn
+
+import (
+	"unicode"
+)
+
+func digit(index int, char rune) (int, bool) {
+	if unicode.IsDigit(char) && index <= 9 {
+		return int(char - '0'), true
+	}
+	if (char == 'x' || char == 'X') && index == 9 {
+		return 10, true
+	}
+	return -1, false
+}
+
+func IsValidISBN(s string) bool {
+	isbn := [10]int{}
+
+	j := 0
+	for _, r := range s {
+		if r == '-' {
+			continue
+		}
+		d, ok := digit(j, r)
+		if !ok {
+			return false
+		}
+		isbn[j] = d
+		j++
+	}
+	if j < 10 {
+		return false
+	}
+
+	check := 0
+	for i, d := range isbn {
+		check += (10 - i) * d
+	}
+	return check%11 == 0
+}
