f25d14ea5439fadd1fd78127e895b29e548b75f0
Sonia Meruelo <smeruelo@gmail.com>
2020-09-27 00:01:34 +0200

    exercism, elixir: word count (solution)

--- exercism/elixir/word-count/lib/word_count.ex
+++ exercism/elixir/word-count/lib/word_count.ex
@@ -1,3 +1,5 @@
+# https://exercism.io/my/solutions/5f8238ec135c478f8e2602905b477898
+
 defmodule WordCount do
   @doc """
   Count the number of words in the sentence.
@@ -6,5 +8,17 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    sentence
+    |> String.downcase()
+    |> String.split(~r{[^[:alnum:]\-]}u, trim: true)
+    |> count_words(%{})
+  end
+
+  @spec count_words([String.t()], map) :: map
+  defp count_words(words, counter) do
+    case words do
+      [] -> counter
+      [head | tail] -> count_words(tail, Map.update(counter, head, 1, fn x -> x + 1 end))
+    end
   end
 end
