28092bc1e4ef58372760fb5b963adae7c1c2ae72
Sonia Meruelo <smeruelo@gmail.com>
2020-09-26 18:38:18 +0200

    exercism, elixir: hello world (solution)

--- exercism/elixir/hello-world/lib/hello_world.ex
+++ exercism/elixir/hello-world/lib/hello_world.ex
@@ -1,9 +1,11 @@
+# https://exercism.io/my/solutions/ea86252553c049328cec34da788b53bd
+
 defmodule HelloWorld do
   @doc """
   Simply returns "Hello, World!"
   """
   @spec hello :: String.t()
   def hello do
-    "Your implementation goes here"
+    "Hello, World!"
   end
 end
