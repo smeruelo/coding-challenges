387ec3835d835cf4a3a37c73b9bc2b62b3726df2
Sonia Meruelo <smeruelo@gmail.com>
2020-09-27 11:07:18 +0200

    exercism, elixir: word count (use Enum.frecuencies)

--- exercism/elixir/word-count/lib/word_count.ex
+++ exercism/elixir/word-count/lib/word_count.ex
@@ -11,14 +11,6 @@ defmodule WordCount do
     sentence
     |> String.downcase()
     |> String.split(~r{[^[:alnum:]\-]}u, trim: true)
-    |> count_words(%{})
-  end
-
-  @spec count_words([String.t()], map) :: map
-  defp count_words(words, counter) do
-    case words do
-      [] -> counter
-      [head | tail] -> count_words(tail, Map.update(counter, head, 1, fn x -> x + 1 end))
-    end
+    |> Enum.frequencies()
   end
 end
