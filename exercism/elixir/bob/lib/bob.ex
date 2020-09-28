9c9f2f7aa8bd3c4c6bc5242c146868223c6e667e
Sonia Meruelo <smeruelo@gmail.com>
2020-09-28 10:11:56 +0200

    exercism, elixir: bob (solution)

--- exercism/elixir/bob/lib/bob.ex
+++ exercism/elixir/bob/lib/bob.ex
@@ -1,7 +1,29 @@
+# https://exercism.io/my/solutions/2966a9d0d2bf4f00bced521ddaebb15c
+
 defmodule Bob do
+  @spec is_question?(String.t()) :: boolean()
+  defp is_question?(sentence) do
+    String.match?(sentence, ~r/\?[[:blank:]]*$/)
+  end
+
+  @spec is_yell?(String.t()) :: boolean()
+  defp is_yell?(sentence) do
+    String.match?(sentence, ~r/[[:upper:]+]/) and not String.match?(sentence, ~r/[[:lower:]+]/)
+  end
+
+  @spec is_empty?(String.t()) :: boolean()
+  defp is_empty?(sentence) do
+    not String.match?(sentence, ~r/[[:alnum:]]+/)
+  end
+
+  @spec hey(String.t()) :: String.t()
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      is_question?(input) and is_yell?(input) -> "Calm down, I know what I'm doing!"
+      is_question?(input) -> "Sure."
+      is_yell?(input) -> "Whoa, chill out!"
+      is_empty?(input) -> "Fine. Be that way!"
+      true -> "Whatever."
     end
   end
 end
