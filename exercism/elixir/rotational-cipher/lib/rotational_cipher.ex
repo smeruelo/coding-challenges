32ef938b813e672a416612e80f9fa4071a8d97f9
Sonia Meruelo <smeruelo@gmail.com>
2020-09-29 22:36:29 +0200

    exercism, elixir: rotational cipher (solution)

--- exercism/elixir/rotational-cipher/lib/rotational_cipher.ex
+++ exercism/elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -1,4 +1,18 @@
+# https://exercism.io/my/solutions/78723a69405a4c7d85051bc817058f23
+
 defmodule RotationalCipher do
+  @alphabet 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  @lower "abcdefghijklmnopqrstuvwxyz"
+  @upper "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  @spec trans_table(shift :: Integer.t()) :: Map.t()
+  defp trans_table(shift) do
+    lower_rotated = String.slice(@lower, shift..-1) <> String.slice(@lower, 0..(shift - 1))
+    upper_rotated = String.slice(@upper, shift..-1) <> String.slice(@upper, 0..(shift - 1))
+    alphabet_rotated = String.to_charlist(lower_rotated <> upper_rotated)
+    Map.new(List.zip([@alphabet, alphabet_rotated]))
+  end
+
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -8,5 +22,7 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    trans = trans_table(shift)
+    text |> String.to_charlist() |> Enum.map(&Map.get(trans, &1, &1)) |> List.to_string()
   end
 end
