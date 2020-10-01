0b903e5fb26debd6f0086a7cd34df16010214751
Sonia Meruelo <smeruelo@gmail.com>
2020-10-01 11:26:04 +0200

    exercism, elixir: beer-song (solution)

--- exercism/elixir/beer-song/lib/beer_song.ex
+++ exercism/elixir/beer-song/lib/beer_song.ex
@@ -1,17 +1,37 @@
+# https://exercism.io/my/solutions/fc842117968b433daa891a425c1aa398
+
 defmodule BeerSong do
+  @spec bottles(integer) :: String.t()
+  defp bottles(0), do: "no more bottles"
+  defp bottles(1), do: "1 bottle"
+  defp bottles(n), do: "#{n} bottles"
+
   @doc """
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
-  def verse(number) do
-    # Your implementation here...
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
   end
+
+  def verse(n) do
+    """
+    #{bottles(n)} of beer on the wall, #{bottles(n)} of beer.
+    Take #{if n === 1, do: "it", else: "one"} down and pass it around, #{bottles(n - 1)} of beer on the wall.
+    """
+  end
+
+  @spec lyrics() :: String.t()
+  def lyrics(), do: lyrics(99..0)
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range) do
-    # Your implementation here...
+    Enum.map_join(range, "\n", &verse(&1))
   end
 end
