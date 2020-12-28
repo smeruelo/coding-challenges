0a245a25f192988c5ce106790391182cad4b04fe
Sonia Meruelo <smeruelo@gmail.com>
2020-12-28 19:57:02 +0100

    exercism, elixir: pig latin (solution)

--- exercism/elixir/pig-latin/lib/pig_latin.ex
+++ exercism/elixir/pig-latin/lib/pig_latin.ex
@@ -1,4 +1,13 @@
+# https://exercism.io/my/solutions/5eab678179104a0799d9aefe7c881615
+
 defmodule PigLatin do
+  @consonants_3 ["squ"]
+  @consonants_2 ["qu"]
+  @consonants_1 String.graphemes("bcdfghjklmnpqrstvwxyz")
+  @vowels_2 Enum.map(@consonants_1, &("x" <> &1)) ++ Enum.map(@consonants_1, &("y" <> &1))
+  @vowels_1 ["a", "e", "i", "o", "u"]
+  @consonant_cluster ~r/^[bcdfghjklmnpqrstvwxyz]*/
+
   @doc """
   Given a `phrase`, translate it a word at a time to Pig Latin.
 
@@ -15,5 +24,23 @@ defmodule PigLatin do
   """
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    String.split(phrase)
+    |> Enum.map(&translate_word/1)
+    |> Enum.join(" ")
+  end
+
+  defp translate_word(word) do
+    cluster = hd(Regex.run(@consonant_cluster, word))
+
+    index =
+      cond do
+        String.starts_with?(word, @vowels_1) -> 0
+        String.starts_with?(word, @vowels_2) -> 0
+        String.starts_with?(word, @consonants_3) -> 3
+        String.starts_with?(word, @consonants_2) -> 2
+        true -> String.length(cluster)
+      end
+
+    String.slice(word, index..String.length(word)) <> String.slice(word, 0, index) <> "ay"
   end
 end
