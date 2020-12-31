8e824e4814d7071093996e23f7d42b3a3885bf89
Sonia Meruelo <smeruelo@gmail.com>
2020-12-31 12:43:24 +0100

    exercism, elixir: robot simulator (solution)

--- exercism/elixir/robot-simulator/lib/robot_simulator.ex
+++ exercism/elixir/robot-simulator/lib/robot_simulator.ex
@@ -1,7 +1,8 @@
+# https://exercism.io/my/solutions/2fdb20b632944a8ab6d168ae551396aa
+
 defmodule RobotSimulator do
   defstruct [:direction, position: {}]
-  @valid_directions [:north, :east, :south, :west]
-  @valid_movements ["R", "L", "A"]
+  @directions [:north, :east, :south, :west]
 
   @doc """
   Create a Robot Simulator given an initial direction and position.
@@ -9,15 +10,12 @@ defmodule RobotSimulator do
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ :north, {x, y} = position \\ {0, 0}) do
-    unless Enum.member?(@valid_directions, direction), do: {:error, "invalid direction"}
-    unless is_integer(x) and is_integer(y), do: {:error, "invalid position"}
-
-    %RobotSimulator{direction: direction, position: position}
-  end
-
-  def create(_, _) do
-    {:error, "invalid position"}
+  def create(direction \\ :north, position \\ {0, 0}) do
+    case {valid_direction?(direction), valid_position?(position)} do
+      {true, true} -> %RobotSimulator{direction: direction, position: position}
+      {false, _} -> {:error, "invalid direction"}
+      {_, false} -> {:error, "invalid position"}
+    end
   end
 
   @doc """
@@ -29,12 +27,13 @@ defmodule RobotSimulator do
   def simulate(robot, instructions) do
     instructions
     |> String.graphemes()
-    |> Enum.reduce(robot, fn movement, r ->
-      unless Enum.member?(@valid_movements, movement),
-        do: {:error, "invalid instruction #{movement}"}
-
-      new_dir = new_direction(direction(r), movement)
-      create(new_dir, new_position(position(r), new_dir))
+    |> Enum.reduce_while(robot, fn mov, r ->
+      case mov do
+        "R" -> {:cont, create(new_direction(r, :right), position(r))}
+        "L" -> {:cont, create(new_direction(r, :left), position(r))}
+        "A" -> {:cont, create(direction(r), new_position(r, :advance))}
+        _ -> {:halt, {:error, "invalid instruction"}}
+      end
     end)
   end
 
@@ -56,8 +55,14 @@ defmodule RobotSimulator do
     robot.position
   end
 
-  defp new_direction(current_direction, _movement = "R") do
-    case current_direction do
+  defp valid_position?({x, y}) when is_integer(x) and is_integer(y), do: true
+
+  defp valid_position?(_), do: false
+
+  defp valid_direction?(direction), do: Enum.member?(@directions, direction)
+
+  defp new_direction(robot, _movement = :right) do
+    case direction(robot) do
       :north -> :east
       :east -> :south
       :south -> :west
@@ -65,8 +70,8 @@ defmodule RobotSimulator do
     end
   end
 
-  defp new_direction(current_direction, _movement = "L") do
-    case current_direction do
+  defp new_direction(robot, _movement = :left) do
+    case direction(robot) do
       :north -> :west
       :east -> :north
       :south -> :east
@@ -74,14 +79,10 @@ defmodule RobotSimulator do
     end
   end
 
-  defp new_direction(current_direction, _movement) do
-    current_direction
-  end
+  defp new_position(robot, _movement = :advance) do
+    {x, y} = position(robot)
 
-  defp new_position(current_position, direction) do
-    {x, y} = current_position
-
-    case direction do
+    case direction(robot) do
       :north -> {x, y + 1}
       :east -> {x + 1, y}
       :south -> {x, y - 1}
