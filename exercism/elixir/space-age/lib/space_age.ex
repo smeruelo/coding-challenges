d56aa236db642b5e74eeb922bd07028fc3c75b58
Sonia Meruelo <smeruelo@gmail.com>
2021-01-01 12:28:40 +0100

    exercism, elixir: space age (solution)

--- exercism/elixir/space-age/lib/space_age.ex
+++ exercism/elixir/space-age/lib/space_age.ex
@@ -9,11 +9,24 @@ defmodule SpaceAge do
           | :uranus
           | :neptune
 
+  @planet_years %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+  @earth_year_seconds 365.25 * 24 * 60 * 60
+
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    seconds / (Map.get(@planet_years, planet) * @earth_year_seconds)
   end
 end
