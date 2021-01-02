05932dfb2cfd4cce38e349626c43013419e2a4fb
Sonia Meruelo <smeruelo@gmail.com>
2021-01-02 12:10:53 +0100

    exercism, elixir: resistor color (solution)

--- exercism/elixir/resistor-color/lib/resistor_color.ex
+++ exercism/elixir/resistor-color/lib/resistor_color.ex
@@ -1,11 +1,27 @@
+# https://exercism.io/my/solutions/6422bca0241c43eea6e1f640d13249bb
+
 defmodule ResistorColor do
   @moduledoc false
 
+  @colors [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+  ]
+  @codes @colors |> Enum.with_index() |> Enum.into(%{})
+
   @spec colors() :: list(String.t())
-  def colors do
-  end
+  def colors, do: @colors
 
   @spec code(String.t()) :: integer()
   def code(color) do
+    Map.get(@codes, color)
   end
 end
